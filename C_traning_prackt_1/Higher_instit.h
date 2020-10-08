1b2804b1357661f1811b4d2c77f6ec4a05d7eebf
GuskoMaksim <gusko-maksim@mail.ru>
2020-10-08 19:07:10 +0300

    Изменения

--- C_traning_prackt_1/Higher_instit.h
+++ C_traning_prackt_1/Higher_instit.h
@@ -26,10 +26,4 @@ public:
 	{
 
 	}
-	virtual void set_average_mark(float) = 0;
-	virtual void set_number_student(int) = 0;
-	virtual float get_average_mark() = 0;
-	virtual int get_number_student() = 0;
-
-
 };
